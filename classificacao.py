32cd58e12da09084cb5a502381ed3bfcf2eeb298
itzigrao <igorperrot006@gmail.com>
2026-07-21 13:33:56 -0300

    Fim aula 13

--- classificacao.py
+++ classificacao.py
@@ -3,7 +3,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-# %%
 df = pd.read_excel("dados/dados_cerveja_nota.xlsx")    
 df.head()
 
@@ -17,5 +16,69 @@ plt.title('cerveja vs notas')
 plt.xlabel('cerveja')
 plt.ylabel('nota')
 
+
+# %%
+
+from sklearn import linear_model
+from sklearn import tree
+from sklearn import naive_bayes
+
+reg = linear_model.LogisticRegression(penalty=None,
+                                       fit_intercept=True)
+reg.fit(df[['cerveja']], df['aprovado'])
+reg_predict = reg.predict(df[['cerveja']].drop_duplicates())
+reg_proba = reg.predict_proba(df[['cerveja']].drop_duplicates())[:,1]
+
+
+arvore_full = tree.DecisionTreeClassifier(random_state=42)
+arvore_full.fit(df[['cerveja']], df['aprovado'])
+arvore_full_predict = arvore_full.predict(df[['cerveja']].drop_duplicates())
+arvore_full_proba = arvore_full.predict_proba(df[['cerveja']].drop_duplicates())[:,1]
+
+arvore_d2 = tree.DecisionTreeClassifier(random_state=42, max_depth=2)
+arvore_d2.fit(df[['cerveja']], df['aprovado'])
+arvore_d2_predict = arvore_d2.predict(df[['cerveja']].drop_duplicates())
+arvore_d2_proba = arvore_d2.predict_proba(df[['cerveja']].drop_duplicates())[:,1]
+
+nb = naive_bayes.GaussianNB()
+nb.fit(df[['cerveja']], df['aprovado'])
+nb_predict = nb.predict(df[['cerveja']].drop_duplicates())
+nb_proba = nb.predict_proba(df[['cerveja']].drop_duplicates())[:,1]
+
+
+
+plt.figure(dpi=400)
+plt.plot(df['cerveja'], df['aprovado'], 'o', color='royalblue')
+plt.grid(True)
+plt.title('cerveja vs notas')
+plt.xlabel('cerveja')
+plt.ylabel('nota')
+
+plt.plot(df['cerveja'].drop_duplicates(), reg_predict, color='tomato')
+#predição da regressão logistica
+plt.plot(df['cerveja'].drop_duplicates(), reg_proba, color='red')
+#valor da curva da regressao logistica
+
+plt.plot(df['cerveja'].drop_duplicates(), arvore_full_predict, color='green')
+plt.plot(df['cerveja'].drop_duplicates(), arvore_full_proba, color='magenta')
+
+plt.plot(df['cerveja'].drop_duplicates(), arvore_d2_predict, color='blue')
+plt.plot(df['cerveja'].drop_duplicates(), arvore_d2_proba, color='black')
+
+plt.plot(df['cerveja'].drop_duplicates(), nb_predict, color='brown')
+plt.plot(df['cerveja'].drop_duplicates(), nb_proba, color='yellow')
+
+plt.hlines(0.5, xmin=1, xmax=9, linestyles='--',colors='black' )
+
+plt.legend(['Observação', 
+            "Reg predict", 
+            "Reg Proba",
+            "Arvore Full Predict",
+            "Arvore Full Proba",
+            "Arvore d2 Predict",
+            "Arvore d2 Proba",
+            "nb predict",
+            "nb proba"
+            ])
 
 # %%
